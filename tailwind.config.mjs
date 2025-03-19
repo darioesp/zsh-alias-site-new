dc6c0f9c42e327b8c32b4001b13741829def116d
Darío Espinoza <dario@fixu.cl>
2025-03-19 11:53:48 -0300

    feat: :sparkles: Add global styles, new fonts, and enhance layout with Hero and Footer components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-import animations from "@midudev/tailwind-animations";
+import animations from '@midudev/tailwind-animations'
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sixtyfour: ['Sixtyfour', 'sans-serif'],
+      },
+      animation: {
+        wave: 'wave 1000ms infinite both',
+      },
+    },
   },
   plugins: [animations],
-  safeList: ["animate-fade-in"],
-};
+  safeList: ['animate-fade-in'],
+}
